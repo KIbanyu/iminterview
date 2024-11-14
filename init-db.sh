753f0f35f1b00086ed5e118db1b1e5a62a14dbe9
Martin Kibanyu <martin.kibanyu@ezra.world>
2024-11-15 00:40:22 +0300

    Added send notifcation from account service

--- init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Run commands as the primary PostgreSQL user
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE db_customer_service;
-    CREATE DATABASE db_transaction_service;
-    CREATE DATABASE db_account_service;
-EOSQL
